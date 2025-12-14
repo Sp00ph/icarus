7f1453f8f27734843c8890f9daa262545d50d706
Markus Everling <markuseverling@gmail.com>
2025-12-14 04:16:22 +0100

    Add position kiwipete

--- src/uci.rs
+++ src/uci.rs
@@ -126,6 +126,10 @@ impl UciCommand {
             "position" => {
                 let startpos = match reader.next() {
                     Some("startpos") => Board::start_pos(),
+                    Some("kiwipete") => Board::read_fen(
+                        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+                    )
+                    .unwrap(),
                     Some("fen") => {
                         let mut fen = String::new();
 
