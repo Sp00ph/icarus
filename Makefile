42e38c4c2ebd3c10b52e7f006a6c9eeda09a6144
Markus Everling <markuseverling@gmail.com>
2026-02-04 22:47:34 +0100

    Move net download logic into a python script.
    Hopefully this causes fewer compile complications.
    
    Bench: 3920269

--- Makefile
+++ Makefile
@@ -3,8 +3,11 @@ EXT =
 ifeq ($(OS),Windows_NT)
 EXT := .exe
 endif
+.PHONY: native
 
-native: export HOST_CC = clang
-native:	
+native:
+ifndef EVALFILE
+	python3 ./download-net.py
+endif
 	cargo +stable build --release -p icarus
 	cp target/release/icarus$(EXT) ./$(EXE)$(EXT)
---
+++ download-net.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import requests
+import hashlib
+
+def main():
+    name, hash = open("network.txt").read().strip().split()
+    path = "./icarus.nnue"
+    try:
+        if hashlib.sha256(open(path, "rb").read()).digest().hex() == hash:
+            print("Net already exists!")
+            return
+    except OSError:
+        pass
+    
+    print(f"Downloading net {name} to {path}")
+    net = requests.get(
+        f"https://github.com/Sp00ph/icarus-nets/releases/download/{name}/{name}.nnue"
+    ).content
+    if hashlib.sha256(net).digest().hex() != hash:
+        print("Invalid hash!")
+        exit(1)
+    open(path, "wb").write(net)
+
+
+if __name__ == "__main__":
+    main()
