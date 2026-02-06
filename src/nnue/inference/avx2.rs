32f14741ddfcdac29845eacce2049465e0bbe3a3
Markus Everling <markuseverling@gmail.com>
2026-02-06 17:25:55 +0100

    Unroll inference
    Bench: 3684181

--- src/nnue/inference/avx2.rs
+++ src/nnue/inference/avx2.rs
@@ -7,37 +7,87 @@ pub fn forward(us: &[i16; HL], them: &[i16; HL], network: &Network) -> i32 {
 
 #[target_feature(enable = "avx2")]
 fn forward_impl(us: &[i16; HL], them: &[i16; HL], network: &Network) -> i32 {
-    assert!(HL.is_multiple_of(16));
+    assert!(HL.is_multiple_of(64));
     let zero = _mm256_setzero_si256();
     let qa = _mm256_set1_epi16(QA);
     let us_weights = network.out_weight.as_ptr();
     let them_weights = network.out_weight[HL..].as_ptr();
 
-    let mut sums = _mm256_setzero_si256();
+    let mut sums0 = _mm256_setzero_si256();
+    let mut sums1 = _mm256_setzero_si256();
+    let mut sums2 = _mm256_setzero_si256();
+    let mut sums3 = _mm256_setzero_si256();
 
-    for i in 0..HL / 16 {
+    for i in 0..HL / 64 {
         unsafe {
-            let us = _mm256_loadu_si256(us.as_ptr().cast::<__m256i>().add(i));
-            let weights = _mm256_loadu_si256(us_weights.cast::<__m256i>().add(i));
-            let us_clamped = _mm256_max_epi16(zero, _mm256_min_epi16(qa, us));
-            sums = _mm256_add_epi32(
-                sums,
-                _mm256_madd_epi16(us_clamped, _mm256_mullo_epi16(us_clamped, weights)),
-            )
+            let us0 = _mm256_loadu_si256(us.as_ptr().cast::<__m256i>().add(4 * i + 0));
+            let us1 = _mm256_loadu_si256(us.as_ptr().cast::<__m256i>().add(4 * i + 1));
+            let us2 = _mm256_loadu_si256(us.as_ptr().cast::<__m256i>().add(4 * i + 2));
+            let us3 = _mm256_loadu_si256(us.as_ptr().cast::<__m256i>().add(4 * i + 3));
+            let weights0 = _mm256_loadu_si256(us_weights.cast::<__m256i>().add(4 * i + 0));
+            let weights1 = _mm256_loadu_si256(us_weights.cast::<__m256i>().add(4 * i + 1));
+            let weights2 = _mm256_loadu_si256(us_weights.cast::<__m256i>().add(4 * i + 2));
+            let weights3 = _mm256_loadu_si256(us_weights.cast::<__m256i>().add(4 * i + 3));
+            let us_clamped0 = _mm256_max_epi16(zero, _mm256_min_epi16(qa, us0));
+            let us_clamped1 = _mm256_max_epi16(zero, _mm256_min_epi16(qa, us1));
+            let us_clamped2 = _mm256_max_epi16(zero, _mm256_min_epi16(qa, us2));
+            let us_clamped3 = _mm256_max_epi16(zero, _mm256_min_epi16(qa, us3));
+            sums0 = _mm256_add_epi32(
+                sums0,
+                _mm256_madd_epi16(us_clamped0, _mm256_mullo_epi16(us_clamped0, weights0)),
+            );
+            sums1 = _mm256_add_epi32(
+                sums1,
+                _mm256_madd_epi16(us_clamped1, _mm256_mullo_epi16(us_clamped1, weights1)),
+            );
+            sums2 = _mm256_add_epi32(
+                sums2,
+                _mm256_madd_epi16(us_clamped2, _mm256_mullo_epi16(us_clamped2, weights2)),
+            );
+            sums3 = _mm256_add_epi32(
+                sums3,
+                _mm256_madd_epi16(us_clamped3, _mm256_mullo_epi16(us_clamped3, weights3)),
+            );
         }
     }
 
-    for i in 0..HL / 16 {
+    for i in 0..HL / 64 {
         unsafe {
-            let them = _mm256_loadu_si256(them.as_ptr().cast::<__m256i>().add(i));
-            let weights = _mm256_loadu_si256(them_weights.cast::<__m256i>().add(i));
-            let them_clamped = _mm256_max_epi16(zero, _mm256_min_epi16(qa, them));
-            sums = _mm256_add_epi32(
-                sums,
-                _mm256_madd_epi16(them_clamped, _mm256_mullo_epi16(them_clamped, weights)),
-            )
+            let them0 = _mm256_loadu_si256(them.as_ptr().cast::<__m256i>().add(4 * i + 0));
+            let them1 = _mm256_loadu_si256(them.as_ptr().cast::<__m256i>().add(4 * i + 1));
+            let them2 = _mm256_loadu_si256(them.as_ptr().cast::<__m256i>().add(4 * i + 2));
+            let them3 = _mm256_loadu_si256(them.as_ptr().cast::<__m256i>().add(4 * i + 3));
+            let weights0 = _mm256_loadu_si256(them_weights.cast::<__m256i>().add(4 * i + 0));
+            let weights1 = _mm256_loadu_si256(them_weights.cast::<__m256i>().add(4 * i + 1));
+            let weights2 = _mm256_loadu_si256(them_weights.cast::<__m256i>().add(4 * i + 2));
+            let weights3 = _mm256_loadu_si256(them_weights.cast::<__m256i>().add(4 * i + 3));
+            let them_clamped0 = _mm256_max_epi16(zero, _mm256_min_epi16(qa, them0));
+            let them_clamped1 = _mm256_max_epi16(zero, _mm256_min_epi16(qa, them1));
+            let them_clamped2 = _mm256_max_epi16(zero, _mm256_min_epi16(qa, them2));
+            let them_clamped3 = _mm256_max_epi16(zero, _mm256_min_epi16(qa, them3));
+            sums0 = _mm256_add_epi32(
+                sums0,
+                _mm256_madd_epi16(them_clamped0, _mm256_mullo_epi16(them_clamped0, weights0)),
+            );
+            sums1 = _mm256_add_epi32(
+                sums1,
+                _mm256_madd_epi16(them_clamped1, _mm256_mullo_epi16(them_clamped1, weights1)),
+            );
+            sums2 = _mm256_add_epi32(
+                sums2,
+                _mm256_madd_epi16(them_clamped2, _mm256_mullo_epi16(them_clamped2, weights2)),
+            );
+            sums3 = _mm256_add_epi32(
+                sums3,
+                _mm256_madd_epi16(them_clamped3, _mm256_mullo_epi16(them_clamped3, weights3)),
+            );
         }
     }
+
+    let sums = _mm256_add_epi32(
+        _mm256_add_epi32(sums0, sums1),
+        _mm256_add_epi32(sums2, sums3),
+    );
 
     let mut output = reduce_sum(sums);
 
--- src/nnue/inference/avx512.rs
+++ src/nnue/inference/avx512.rs
@@ -7,37 +7,87 @@ pub fn forward(us: &[i16; HL], them: &[i16; HL], network: &Network) -> i32 {
 
 #[target_feature(enable = "avx512bw")]
 fn forward_impl(us: &[i16; HL], them: &[i16; HL], network: &Network) -> i32 {
-    assert!(HL.is_multiple_of(32));
+    assert!(HL.is_multiple_of(128));
     let zero = _mm512_setzero_si512();
     let qa = _mm512_set1_epi16(QA);
     let us_weights = network.out_weight.as_ptr();
     let them_weights = network.out_weight[HL..].as_ptr();
 
-    let mut sums = _mm512_setzero_si512();
+    let mut sums0 = _mm512_setzero_si512();
+    let mut sums1 = _mm512_setzero_si512();
+    let mut sums2 = _mm512_setzero_si512();
+    let mut sums3 = _mm512_setzero_si512();
 
-    for i in 0..HL / 32 {
+    for i in 0..HL / 128 {
         unsafe {
-            let us = _mm512_loadu_si512(us.as_ptr().cast::<__m512i>().add(i));
-            let weights = _mm512_loadu_si512(us_weights.cast::<__m512i>().add(i));
-            let us_clamped = _mm512_max_epi16(zero, _mm512_min_epi16(qa, us));
-            sums = _mm512_add_epi32(
-                sums,
-                _mm512_madd_epi16(us_clamped, _mm512_mullo_epi16(us_clamped, weights)),
-            )
+            let us0 = _mm512_loadu_si512(us.as_ptr().cast::<__m512i>().add(4 * i + 0));
+            let us1 = _mm512_loadu_si512(us.as_ptr().cast::<__m512i>().add(4 * i + 1));
+            let us2 = _mm512_loadu_si512(us.as_ptr().cast::<__m512i>().add(4 * i + 2));
+            let us3 = _mm512_loadu_si512(us.as_ptr().cast::<__m512i>().add(4 * i + 3));
+            let weights0 = _mm512_loadu_si512(us_weights.cast::<__m512i>().add(4 * i + 0));
+            let weights1 = _mm512_loadu_si512(us_weights.cast::<__m512i>().add(4 * i + 1));
+            let weights2 = _mm512_loadu_si512(us_weights.cast::<__m512i>().add(4 * i + 2));
+            let weights3 = _mm512_loadu_si512(us_weights.cast::<__m512i>().add(4 * i + 3));
+            let us_clamped0 = _mm512_max_epi16(zero, _mm512_min_epi16(qa, us0));
+            let us_clamped1 = _mm512_max_epi16(zero, _mm512_min_epi16(qa, us1));
+            let us_clamped2 = _mm512_max_epi16(zero, _mm512_min_epi16(qa, us2));
+            let us_clamped3 = _mm512_max_epi16(zero, _mm512_min_epi16(qa, us3));
+            sums0 = _mm512_add_epi32(
+                sums0,
+                _mm512_madd_epi16(us_clamped0, _mm512_mullo_epi16(us_clamped0, weights0)),
+            );
+            sums1 = _mm512_add_epi32(
+                sums1,
+                _mm512_madd_epi16(us_clamped1, _mm512_mullo_epi16(us_clamped1, weights1)),
+            );
+            sums2 = _mm512_add_epi32(
+                sums2,
+                _mm512_madd_epi16(us_clamped2, _mm512_mullo_epi16(us_clamped2, weights2)),
+            );
+            sums3 = _mm512_add_epi32(
+                sums3,
+                _mm512_madd_epi16(us_clamped3, _mm512_mullo_epi16(us_clamped3, weights3)),
+            );
         }
     }
 
-    for i in 0..HL / 32 {
+    for i in 0..HL / 128 {
         unsafe {
-            let them = _mm512_loadu_si512(them.as_ptr().cast::<__m512i>().add(i));
-            let weights = _mm512_loadu_si512(them_weights.cast::<__m512i>().add(i));
-            let them_clamped = _mm512_max_epi16(zero, _mm512_min_epi16(qa, them));
-            sums = _mm512_add_epi32(
-                sums,
-                _mm512_madd_epi16(them_clamped, _mm512_mullo_epi16(them_clamped, weights)),
-            )
+            let them0 = _mm512_loadu_si512(them.as_ptr().cast::<__m512i>().add(4 * i + 0));
+            let them1 = _mm512_loadu_si512(them.as_ptr().cast::<__m512i>().add(4 * i + 1));
+            let them2 = _mm512_loadu_si512(them.as_ptr().cast::<__m512i>().add(4 * i + 2));
+            let them3 = _mm512_loadu_si512(them.as_ptr().cast::<__m512i>().add(4 * i + 3));
+            let weights0 = _mm512_loadu_si512(them_weights.cast::<__m512i>().add(4 * i + 0));
+            let weights1 = _mm512_loadu_si512(them_weights.cast::<__m512i>().add(4 * i + 1));
+            let weights2 = _mm512_loadu_si512(them_weights.cast::<__m512i>().add(4 * i + 2));
+            let weights3 = _mm512_loadu_si512(them_weights.cast::<__m512i>().add(4 * i + 3));
+            let them_clamped0 = _mm512_max_epi16(zero, _mm512_min_epi16(qa, them0));
+            let them_clamped1 = _mm512_max_epi16(zero, _mm512_min_epi16(qa, them1));
+            let them_clamped2 = _mm512_max_epi16(zero, _mm512_min_epi16(qa, them2));
+            let them_clamped3 = _mm512_max_epi16(zero, _mm512_min_epi16(qa, them3));
+            sums0 = _mm512_add_epi32(
+                sums0,
+                _mm512_madd_epi16(them_clamped0, _mm512_mullo_epi16(them_clamped0, weights0)),
+            );
+            sums1 = _mm512_add_epi32(
+                sums1,
+                _mm512_madd_epi16(them_clamped1, _mm512_mullo_epi16(them_clamped1, weights1)),
+            );
+            sums2 = _mm512_add_epi32(
+                sums2,
+                _mm512_madd_epi16(them_clamped2, _mm512_mullo_epi16(them_clamped2, weights2)),
+            );
+            sums3 = _mm512_add_epi32(
+                sums3,
+                _mm512_madd_epi16(them_clamped3, _mm512_mullo_epi16(them_clamped3, weights3)),
+            );
         }
     }
+
+    let sums = _mm512_add_epi32(
+        _mm512_add_epi32(sums0, sums1),
+        _mm512_add_epi32(sums2, sums3),
+    );
 
     let mut output = reduce_sum(sums);
 
