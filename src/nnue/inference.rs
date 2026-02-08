d6aeaf1107aa2a7bc199ac17784bbe433fe1ab9d
Markus Everling <markuseverling@gmail.com>
2026-02-08 12:23:24 +0100

    Temporarily crash non AVX2 builds for the SPRT
    Bench: 3728274

--- src/nnue/inference.rs
+++ src/nnue/inference.rs
@@ -17,6 +17,7 @@ cfg_if!(
         mod avx2;
         pub use avx2::forward;
     } else {
+        error!("Non avx2 disabled for SPRT purposes");
         mod generic;
         pub use generic::forward;
     }
