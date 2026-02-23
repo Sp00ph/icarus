62f09326788da8a6102795f4f98f04bea2f89f34
Sp00ph <61327188+Sp00ph@users.noreply.github.com>
2026-02-24 00:53:05 +0100

    Non PV LMR (#39)
    ```
    Elo   | 5.70 +- 3.91 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 8902 W: 2506 L: 2360 D: 4036
    Penta | [49, 1019, 2189, 1125, 69]
    ```
    https://chess.n9x.co/test/5792/
    
    Bench: 3074736

--- src/search/search.rs
+++ src/search/search.rs
@@ -224,7 +224,7 @@ pub fn search<Node: NodeType>(
         }
 
         let is_tactic = pos.board().is_tactic(mv);
-        let lmr = get_lmr(is_tactic, depth as u8, moves_seen);
+        let mut lmr = get_lmr(is_tactic, depth as u8, moves_seen);
         let mut extension = 0;
         let mut score;
 
@@ -324,6 +324,12 @@ pub fn search<Node: NodeType>(
         if moves_seen == 0 {
             score = -search::<Node::Next>(pos, new_depth, ply + 1, -beta, -alpha, thread);
         } else {
+            if depth < 2 {
+                lmr = 0;
+            } else {
+                lmr += !Node::PV as i16;
+            }
+
             let lmr_depth = (new_depth - lmr).max(1).min(new_depth);
 
             score = -search::<NonPV>(pos, lmr_depth, ply + 1, -alpha - 1, -alpha, thread);
