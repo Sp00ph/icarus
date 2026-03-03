942f63e7d9195689e02e979d80419c2a2fc429a1
Markus Everling <markuseverling@gmail.com>
2026-03-03 16:31:56 +0100

    Interpolate between fromto and pieceto for main hist
    Bench: 3160778

--- src/search/history/main.rs
+++ src/search/history/main.rs
@@ -4,7 +4,9 @@ use crate::search::history::{MAX_HIST_VALUE, apply_gravity};
 
 pub struct MainHist {
     /// [stm][from][from attacked][to][to attacked]
-    data: [[[[[i16; 2]; 64]; 2]; 64]; 2],
+    from_to: [[[[[i16; 2]; 64]; 2]; 64]; 2],
+    /// [stm][piece][from attacked][to][to attacked]
+    piece_to: [[[[[i16; 2]; 64]; 2]; 6]; 2],
 }
 
 impl MainHist {
@@ -23,31 +25,62 @@ impl MainHist {
     }
 
     pub fn get(&self, board: &Board, mv: Move) -> i16 {
-        let (stm, from, to) = (board.stm(), mv.from(), mv.to());
-        let from_threatened = board.attacked().contains(from) as usize;
-        let to_threatened = board.attacked().contains(to) as usize;
-
-        self.data[stm][from][from_threatened][to][to_threatened]
+        self.get_from_to(board, mv)
+            .midpoint(self.get_piece_to(board, mv))
     }
 
-    fn get_mut(&mut self, board: &Board, mv: Move) -> &mut i16 {
+    fn get_from_to(&self, board: &Board, mv: Move) -> i16 {
         let (stm, from, to) = (board.stm(), mv.from(), mv.to());
         let from_threatened = board.attacked().contains(from) as usize;
         let to_threatened = board.attacked().contains(to) as usize;
 
-        &mut self.data[stm][from][from_threatened][to][to_threatened]
+        self.from_to[stm][from][from_threatened][to][to_threatened]
+    }
+
+    fn get_from_to_mut(&mut self, board: &Board, mv: Move) -> &mut i16 {
+        let (stm, from, to) = (board.stm(), mv.from(), mv.to());
+        let from_threatened = board.attacked().contains(from) as usize;
+        let to_threatened = board.attacked().contains(to) as usize;
+
+        &mut self.from_to[stm][from][from_threatened][to][to_threatened]
+    }
+
+    fn get_piece_to(&self, board: &Board, mv: Move) -> i16 {
+        let (stm, from, to) = (board.stm(), mv.from(), mv.to());
+        let piece = board.piece_on(from).unwrap();
+        let from_threatened = board.attacked().contains(from) as usize;
+        let to_threatened = board.attacked().contains(to) as usize;
+
+        self.piece_to[stm][piece][from_threatened][to][to_threatened]
+    }
+
+    fn get_piece_to_mut(&mut self, board: &Board, mv: Move) -> &mut i16 {
+        let (stm, from, to) = (board.stm(), mv.from(), mv.to());
+        let piece = board.piece_on(from).unwrap();
+        let from_threatened = board.attacked().contains(from) as usize;
+        let to_threatened = board.attacked().contains(to) as usize;
+
+        &mut self.piece_to[stm][piece][from_threatened][to][to_threatened]
     }
 
     pub fn apply_bonus(&mut self, board: &Board, mv: Move, depth: i16) {
         apply_gravity::<MAX_HIST_VALUE, MAX_HIST_VALUE>(
-            self.get_mut(board, mv),
+            self.get_from_to_mut(board, mv),
+            Self::bonus(depth),
+        );
+        apply_gravity::<MAX_HIST_VALUE, MAX_HIST_VALUE>(
+            self.get_piece_to_mut(board, mv),
             Self::bonus(depth),
         );
     }
 
     pub fn apply_malus(&mut self, board: &Board, mv: Move, depth: i16) {
         apply_gravity::<MAX_HIST_VALUE, MAX_HIST_VALUE>(
-            self.get_mut(board, mv),
+            self.get_from_to_mut(board, mv),
+            -Self::malus(depth),
+        );
+        apply_gravity::<MAX_HIST_VALUE, MAX_HIST_VALUE>(
+            self.get_piece_to_mut(board, mv),
             -Self::malus(depth),
         );
     }
