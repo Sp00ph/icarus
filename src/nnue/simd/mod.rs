816e7aabae83bd80698a700519e691bbc02312c3
Markus Everling <markuseverling@gmail.com>
2026-05-12 16:32:00 +0200

    neon fr this time

--- src/nnue/simd/mod.rs
+++ src/nnue/simd/mod.rs
@@ -7,6 +7,10 @@ cfg_select! {
         mod avx2;
         pub use avx2::*;
     }
+    target_feature = "neon" => {
+        mod neon;
+        pub use neon::*;
+    }
     _ => {
         compile_error!("Unsupported arch!");
     },
